44e013a26186e9534ccfa24e80380d29882f6ace
Julian Rodriguez <92952502+juleanrod@users.noreply.github.com>
2022-11-20 13:46:34 -0800

    Add file handling and routing

--- src/app.js
+++ src/app.js
@@ -13,9 +13,43 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 //Create the Index View File
 
+// Read and store accounts.json data
+const accounts = fs.readFileSync(path.join(__dirname, 'json', 'accounts.json'), { encoding: 'utf8' }, (err, data) => {
+    if(err) console.log(err);
+    console.log('accounts.json was succesfully read');
+    return JSON.parse(data);
+});
+
+// Read and store users.json data
+const users = fs.readFileSync(path.join(__dirname, 'json', 'users.json'), { encoding: 'utf8' }, (err, data) => {
+    if(err) console.log(err);
+    console.log('users.json was succesfully read');
+    return JSON.parse(data);
+});
+
 // Create the Index Route
 app.get('/', (req, res) => {
-    res.render('index', { title: 'Index' });
+    res.render('index', { title: 'Accounts Summary', accounts: accounts });
+});
+
+// Create the Savings Account Route
+app.get('/savings', (req, res) => {
+    res.render('account',  { account: accounts.savings })
+});
+
+// Create the Checking Account Route
+app.get('/checking', (req, res) => {
+    res.render('checking',  { account: accounts.checking })
+});
+
+// Create the Credit Account Route
+app.get('/credit', (req, res) => {
+    res.render('credit',  { account: accounts.credit })
+});
+
+// Create the Credit Account Route
+app.get('/profile', (req, res) => {
+    res.render('profile',  { user: users[0] })
 });
 
 app.listen(3000, (err) => {
